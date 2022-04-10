5ec8fa723d0da495991bc314bd214b29504b28b2
at350 <46613976+at350@users.noreply.github.com>
2022-04-09 18:49:51 -0700

    Music and animation changes

---
+++ formscript.js
@@ -0,0 +1,6 @@
+// const form = document.querySelector('form');
+// const input = document.getElementById("mins");
+
+// form.addEventListener('submit', () => {
+//     input.value = '';
+// })
--- script.js
+++ script.js
@@ -1,11 +1,452 @@
 const yogaPoses = [
     {
+        name: 'Big Toe Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2007/08/big-toe-pose.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Chair Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/11/Chair-Pose_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Dolphin Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2007/08/hp_263_10_bjk_fnl.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Eagle Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/12/Eagle-Pose_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Extended Side Angle Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/11/Extended-Side-Angle_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Warrior II Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/12/Warrior-2_Andrew-Clark_1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'High Lunge',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2008/02/ginacaputohighlungeposepage.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Upward Salute',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2008/02/upward-salute-primary.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Bridge Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/11/Bridge-Pose_Andrew-Clark_1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Warrior I Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/10/Warrior-1_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Corpse Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/10/Savasana_Andrew-Clark_1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Standing Half Forward Bend',
+        imgURL: 'https://www.yogajournal.com/poses/standing-half-forward-bend/',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Legs Up the Wall Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/12/Legs-Up-the-Wall_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Pyramid Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2007/08/ccd04294.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Boat Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/10/Boat-Pose1_Andrew-Clark-1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Happy Baby Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2008/05/jan-15-home-practice-ananda-balasana-happy-baby-pose.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Side Plank Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/11/Side-Plank-Pose_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Chair Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/11/Chair-Pose_Andrew-Clark.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Bound Angle Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2021/12/Bound-Angle-Pose-Mod-1_Andrew-Clark_1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Head-to-Knee Pose',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2007/08/Janu-Sirsasana_Andrew-Clark_1.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Standing Split',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2008/06/two-fit-moms-in-standing-split.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: 'Wide-Legged Standing Forward Bend',
+        imgURL: 'https://www.yogajournal.com/wp-content/uploads/2007/08/wide-legged-standing-forward-bend-primary.jpg?crop=535:301&width=1070&enable=upscale',
+        chair: false,
+        difficulty: 0
+    },
+    {
         name: '',
         imgURL: '',
         chair: false,
         difficulty: 0
     },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    },
+    {
+        name: '',
+        imgURL: '',
+        chair: false,
+        difficulty: 0
+    }
 ]
+
+const muteBtn = document.querySelector('#mute');
+let isPlay = true;
+muteBtn.addEventListener('click', () => {
+    if (isPlay) {
+        stop();
+    } else {
+        start();
+    }
+    isPlay = !isPlay;
+});
+
+const music = document.querySelector('audio');
+
+const stop = () => {
+    music.pause();
+}
+
+const start = () => {
+    music.play();
+}
 
 const breakT = [5,10,15,20,25];
 const breakLeast = 5;
@@ -20,14 +461,15 @@ let isBreak = false;
 
 const submitbtn = document.querySelector('submitbt');
 const chair = document.querySelectorAll('input[name = "has_chair"'); 
-const time = document.getElementById("mins");
+const time = document.getElementById("mins").value;
 const intensity =  document.querySelectorAll('input[name = "intensity"');
 
 // const form = document.querySelector('.inputform');
+// console.log(form);
 // form.addEventListener('submit', (e) => {
-//     const minutes = document.getElementById("mins");
-    
+//     time.value = '';
 // })
+
 let windowURL = `${window.location.href}`;
 let params = new URLSearchParams(windowURL.slice(windowURL.indexOf('?')));
 let userInput;
@@ -80,10 +522,19 @@ const initTimer = setInterval(()=> {
     }
 }, 1000);
 
+yogaPoses = shuffle(yogaPoses);
+
+let curIndex = 0;
+
 let showYoga = function(start) {
     counter = timeList[start];
-    if (isBreak) yogaPos.innerText = 'BREAK TIME';
-    else yogaPos.innerText = 'THE YOGA POSITION';
+    if (isBreak) {
+        yogaPos.innerText = 'BREAK TIME';
+        yogaImg
+    }
+    else {
+        yogaPos.innerText = 'THE YOGA POSITION';
+    }
     const timer = setInterval(() => {
         timeRemain.innerText = `Time Remaining: ${counter}`
         counter--;
@@ -103,6 +554,24 @@ let showYoga = function(start) {
 let endYoga = () => {
 
 };
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
 
 // for (let i=0; i<timeList.length; i++) {
 //     if (isBreak) {
